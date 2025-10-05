cbf19f4cc7f968e840a9e5293155c71f56f5d0bb
Pavel Khamenia <khameniapavel@gmail.com>
2025-10-05 15:43:21 +0300

    Update project

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -7,7 +7,6 @@ import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.FileDownloadMode;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
-import org.assertj.core.util.Arrays;
 import org.testng.ITestContext;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
@@ -29,6 +28,8 @@ import pages.ShoesPage;
 import pages.WorkoutDetailsPage;
 import utils.PropertyReader;
 import utils.TestListener;
+
+import java.util.Arrays;
 
 @Listeners({TestListener.class})
 public class BaseTest {
@@ -57,7 +58,35 @@ public class BaseTest {
         Configuration.timeout = 10000;
         Configuration.fileDownload = FileDownloadMode.FOLDER;
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide().screenshots(true).savePageSource(true));
+    }
 
+    @BeforeMethod(alwaysRun = true)
+    public void preCondition() {
+        // Инициализируем страницы ПЕРЕД использованием
+        initializePages();
+
+        open("/");
+        loginPage.login(BASE_LOGIN, BASE_PASSWORD);
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void cleanup(ITestResult result) {
+        try {
+            // Выполняем очистку только если браузер еще открыт
+            if (!isWebDriverClosed()) {
+                performGroupSpecificCleanup(result);
+            }
+        } catch (Exception e) {
+            System.err.println("Error during cleanup: " + e.getMessage());
+        } finally {
+            // Всегда закрываем браузер в конце
+            if (!isWebDriverClosed()) {
+                closeWebDriver();
+            }
+        }
+    }
+
+    private void initializePages() {
         this.loginPage = new LoginPage();
         this.calendarPage = new CalendarPage();
         this.addWorkoutPage = new AddWorkoutPage();
@@ -71,31 +100,23 @@ public class BaseTest {
         this.bikesPage = new BikesPage();
     }
 
-    @BeforeMethod(alwaysRun = true)
-    public void preCondition() {
-        open("/");
-        // Инициализация страниц при необходимости
-        this.loginPage = new LoginPage();
-        loginPage.login(BASE_LOGIN, BASE_PASSWORD);
-    }
+    private void performGroupSpecificCleanup(ITestResult result) {
+        String[] groups = result.getMethod().getGroups();
 
-    @AfterMethod(alwaysRun = true)
-    public void cleanup(ITestResult result) {
         try {
-            // Определяем порядок очистки явно
-            if (isTestInGroup(result, "shoesDelete")) {
+            if (Arrays.asList(groups).contains("shoesDelete")) {
                 shoesPage.deleteShoesWithConfirmation();
             }
-            if (isTestInGroup(result, "workoutDelete")) {
+            if (Arrays.asList(groups).contains("workoutDelete")) {
                 workoutDetailsPage.clickWorkoutActionsDropdown();
                 addWorkoutPage.deleteWorkout();
             }
-            if (isTestInGroup(result, "workoutDeleteToday")) {
+            if (Arrays.asList(groups).contains("workoutDeleteToday")) {
                 calendarPage.deleteTodayWorkout();
             }
-        } finally {
-            // Всегда закрываем браузер в конце
-            closeWebDriver();
+        } catch (Exception e) {
+            System.err.println("Error in group-specific cleanup: " + e.getMessage());
+            // Продолжаем выполнение, даже если одна из операций очистки не удалась
         }
     }
 
@@ -103,8 +124,13 @@ public class BaseTest {
         return Arrays.asList(result.getMethod().getGroups()).contains(group);
     }
 
-    @AfterMethod(alwaysRun = true)
-    public void postCondition() {
-        closeWebDriver();
+    private boolean isWebDriverClosed() {
+        try {
+            // Попробуем получить текущий URL чтобы проверить, открыт ли еще браузер
+            com.codeborne.selenide.WebDriverRunner.getWebDriver();
+            return false;
+        } catch (Exception e) {
+            return true;
+        }
     }
 }
