1287b64d4f621fe5699afd726bded235376302a6
Pavel Khamenia <khameniapavel@gmail.com>
2025-10-05 21:03:16 +0300

    Update project

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -2,12 +2,16 @@ package tests;
 
 import static com.codeborne.selenide.Selenide.closeWebDriver;
 import static com.codeborne.selenide.Selenide.open;
+import static utils.AllureUtils.takeScreenshot;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.FileDownloadMode;
+import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.Attachment;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.assertj.core.util.Arrays;
+import org.openqa.selenium.OutputType;
 import org.testng.ITestContext;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
@@ -32,69 +36,83 @@ import utils.TestListener;
 
 @Listeners({TestListener.class})
 public class BaseTest {
-    protected static final String BASE_LOGIN = PropertyReader.getProperty("login");
-    protected static final String BASE_PASSWORD = PropertyReader.getProperty("password");
-    protected String BASE_URL = PropertyReader.getProperty("url");
-    protected LoginPage loginPage;
-    protected CalendarPage calendarPage;
-    protected AddWorkoutPage addWorkoutPage;
-    protected DashboardPage dashboardPage;
-    protected WorkoutDetailsPage workoutDetailsPage;
-    protected ReportPage reportPage;
-    protected CalculatorPage calculatorPage;
-    protected LogoutPage logoutPage;
-    protected EquipmentPage equipmentPage;
-    protected ShoesPage shoesPage;
-    protected BikesPage bikesPage;
 
-    @BeforeClass(alwaysRun = true)
-    @Parameters({"browserName"})
-    public void setUp(@Optional("chrome") String browser, ITestContext testContext) {
-        Configuration.baseUrl = BASE_URL;
-        Configuration.browserSize = "1920x1080";
-        Configuration.browser = browser;
-        Configuration.headless = Boolean.parseBoolean(PropertyReader.getProperty("headless"));
-        Configuration.timeout = 10000;
-        Configuration.fileDownload = FileDownloadMode.FOLDER;
-        SelenideLogger.addListener("AllureSelenide", new AllureSelenide().screenshots(true).savePageSource(true));
+  protected static final String BASE_LOGIN = PropertyReader.getProperty("login");
+  protected static final String BASE_PASSWORD = PropertyReader.getProperty("password");
+  protected String BASE_URL = PropertyReader.getProperty("url");
+  protected LoginPage loginPage;
+  protected CalendarPage calendarPage;
+  protected AddWorkoutPage addWorkoutPage;
+  protected DashboardPage dashboardPage;
+  protected WorkoutDetailsPage workoutDetailsPage;
+  protected ReportPage reportPage;
+  protected CalculatorPage calculatorPage;
+  protected LogoutPage logoutPage;
+  protected EquipmentPage equipmentPage;
+  protected ShoesPage shoesPage;
+  protected BikesPage bikesPage;
 
-        this.loginPage = new LoginPage();
-        this.calendarPage = new CalendarPage();
-        this.addWorkoutPage = new AddWorkoutPage();
-        this.dashboardPage = new DashboardPage();
-        this.workoutDetailsPage = new WorkoutDetailsPage();
-        this.reportPage = new ReportPage();
-        this.calculatorPage = new CalculatorPage();
-        this.logoutPage = new LogoutPage();
-        this.equipmentPage = new EquipmentPage();
-        this.shoesPage = new ShoesPage();
-        this.bikesPage = new BikesPage();
+  @BeforeClass(alwaysRun = true)
+  @Parameters({"browserName"})
+  public void setUp(@Optional("chrome") String browser, ITestContext testContext) {
+    Configuration.baseUrl = BASE_URL;
+    Configuration.browserSize = "1920x1080";
+    Configuration.browser = browser;
+    Configuration.headless = Boolean.parseBoolean(PropertyReader.getProperty("headless"));
+    Configuration.timeout = 10000;
+    Configuration.fileDownload = FileDownloadMode.FOLDER;
+    SelenideLogger.addListener("AllureSelenide",
+        new AllureSelenide().screenshots(true).savePageSource(true));
+
+    this.loginPage = new LoginPage();
+    this.calendarPage = new CalendarPage();
+    this.addWorkoutPage = new AddWorkoutPage();
+    this.dashboardPage = new DashboardPage();
+    this.workoutDetailsPage = new WorkoutDetailsPage();
+    this.reportPage = new ReportPage();
+    this.calculatorPage = new CalculatorPage();
+    this.logoutPage = new LogoutPage();
+    this.equipmentPage = new EquipmentPage();
+    this.shoesPage = new ShoesPage();
+    this.bikesPage = new BikesPage();
+  }
+
+  //@BeforeMethod(alwaysRun = true)
+  public void preCondition() {
+    open("/");
+    loginPage.login(BASE_LOGIN, BASE_PASSWORD);
+  }
+
+  //@AfterMethod(onlyForGroups = "shoesDelete", alwaysRun = true)
+  public void deleteShoes() {
+    shoesPage.deleteShoesWithConfirmation();
+  }
+
+  //@AfterMethod(onlyForGroups = "workoutDelete", alwaysRun = true)
+  public void deleteWorkout() {
+    workoutDetailsPage.clickWorkoutActionsDropdown();
+    addWorkoutPage.deleteWorkout();
+  }
+
+  //@AfterMethod(onlyForGroups = "workoutDeleteToday", alwaysRun = true)
+  public void deleteTodayWorkout() {
+    calendarPage.deleteTodayWorkout();
+  }
+
+  @AfterMethod(alwaysRun = true)
+  public void postCondition() {
+    closeWebDriver();
+  }
+
+  @AfterMethod(alwaysRun = true)
+  public void teardown(ITestResult result) {
+    if (result.getStatus() == ITestResult.FAILURE) {
+      takeScreenshot();
     }
+  }
 
-    //@BeforeMethod(alwaysRun = true)
-    public void preCondition() {
-        open("/");
-        loginPage.login(BASE_LOGIN, BASE_PASSWORD);
-    }
-
-    //@AfterMethod(onlyForGroups = "shoesDelete", alwaysRun = true)
-    public void deleteShoes() {
-        shoesPage.deleteShoesWithConfirmation();
-    }
-
-    //@AfterMethod(onlyForGroups = "workoutDelete", alwaysRun = true)
-    public void deleteWorkout() {
-        workoutDetailsPage.clickWorkoutActionsDropdown();
-        addWorkoutPage.deleteWorkout();
-    }
-
-    //@AfterMethod(onlyForGroups = "workoutDeleteToday", alwaysRun = true)
-    public void deleteTodayWorkout() {
-        calendarPage.deleteTodayWorkout();
-    }
-
-    @AfterMethod(alwaysRun = true)
-    public void postCondition() {
-        closeWebDriver();
-    }
+  @Attachment(value = "Screenshot on failure", type = "image/png")
+  public byte[] takeScreenshot() {
+    return Selenide.screenshot(OutputType.BYTES);
+  }
 }
